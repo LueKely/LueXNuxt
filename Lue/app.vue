9f007d2c2a58ef088ae2c2939074da73ab732f1e
LueKely <luekely43110@gmail.com>
2022-12-19 09:49:42 +0800

    -add tailwind plug in and config

--- Lue/app.vue
+++ Lue/app.vue
@@ -1,5 +1,11 @@
 <template>
 	<div>
-		<NuxtWelcome />
+		<!-- <NuxtWelcome /> -->
+		<p class="bg-pin w-10 h-10">{{ lue }}</p>
 	</div>
 </template>
+<script setup lang="ts">
+	import { ref } from 'vue';
+
+	const lue = ref<number>(5);
+</script>
---
+++ Lue/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+	important: true,
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: { colors: { pin: '#ff49db' }, extend: {} },
+	plugins: [],
+};
