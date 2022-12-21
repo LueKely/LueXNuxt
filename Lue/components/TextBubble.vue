3dca0cda6e17ee12ccea6ac67e293d98716edc06
LueKely <luekely43110@gmail.com>
2022-12-21 15:09:50 +0800

    moved  textbubble as component

---
+++ Lue/components/TextBubble.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="w-screen h-screen flex items-center justify-center">
+		<!-- <NuxtWelcome /> -->
+
+		<!-- container -->
+		<div
+			class="w-50vw] h-[50vh] bg-neutral-400 flex items-center justify-start"
+		>
+			<!-- container -->
+			<div
+				class="w-[500px] h-[500px] bg-lime-300 flex items-center justify-center relative"
+			>
+				<!-- box -->
+				<div
+					ref="textbox"
+					class="bg-red-500 w-[400px] h-[400px] z-10 relative"
+				></div>
+				<!-- shadow -->
+				<div class="bg-neutral-800 w-[400px] h-[400px] absolute test z-0"></div>
+			</div>
+		</div>
+	</div>
+</template>
+<script setup lang="ts">
+	const textbox = ref(null);
+	const {
+		elementX,
+		elementY,
+
+		elementWidth,
+		elementHeight,
+	} = useMouseInElement(textbox);
+
+	const distanceX = computed(() => {
+		return (elementX.value / 10).toFixed(1);
+	});
+	const distanceY = computed(() => {
+		return (elementY.value / 8).toFixed(1);
+	});
+
+	const yToString = computed(() => {
+		return `${distanceY.value}px`;
+	});
+
+	const xToString = computed(() => {
+		return `${distanceX.value}px`;
+	});
+	// lagay if statement na kung lagpas na sa threshhold gawing static nalang pero yeah change nalang values kung
+</script>
+<style scoped>
+	.test {
+		right: v-bind('xToString');
+		bottom: v-bind('yToString');
+		transition: all ease-in-out;
+		transition-duration: 50ms;
+	}
+</style>
