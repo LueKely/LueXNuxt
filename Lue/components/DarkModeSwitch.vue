061cca415a18aa609b1a37cff8407679dc2bc046
LueKely <luekely43110@gmail.com>
2022-12-26 08:14:28 +0800

    fixed bug

--- Lue/components/DarkModeSwitch.vue
+++ Lue/components/DarkModeSwitch.vue
@@ -20,12 +20,8 @@
 				class="transition-transform ease-in-out duration-100 w-11 h-5 border-[3px] border-neutral-900 rounded-full bg-neutral-100 dark:bg-neutral-900 dark:border-neutral-50"
 			></div>
 			<div
-				:class="{
-					moveRight: isDark == true,
-					moveLeft: isDark == false,
-				}"
 				@click="clickNightSwitch"
-				class="transition-transform duration-100 ease-in-out h-6 w-6 border-[3px] border-neutral-900 rounded-full absolute bg-neutral-50 hover:cursor-pointer dark:bg-neutral-900 dark:border-neutral-50"
+				class="transition-transform translate-x-[-12px] dark:translate-x-3 duration-100 ease-in-out h-6 w-6 border-[3px] border-neutral-900 rounded-full absolute bg-neutral-50 hover:cursor-pointer dark:bg-neutral-900 dark:border-neutral-50"
 			></div>
 		</div>
 		<Icon
