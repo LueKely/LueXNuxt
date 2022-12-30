8c3771fa1955b01b5a6640cec6e3cc7099fbf078
LueKely <luekely43110@gmail.com>
2022-12-30 10:19:28 +0800

    deleted some files

--- Lue/components/TextFile.vue
+++ Lue/components/TextFile.vue
@@ -115,7 +115,4 @@
 	.TextFileShadow {
 		box-shadow: 15px 19px 0px 0px;
 	}
-	.stylebg {
-		background-image: url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='5' height='5' patternTransform='scale(1) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0,0%,100%,1)'/><path d='M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5'  stroke-width='1' stroke='none' fill='hsla(0, 0%, 0%, 1)'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>");
-	}
 </style>
