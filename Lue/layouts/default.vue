4c9e5b2baf355764c5fbfbb6c3e6d9ec2aaf44b6
LueKely <luekely43110@gmail.com>
2023-01-05 09:29:37 +0800

    Added: Textures
    I added the 2 svg's needed for the textures to make it more better for dark and light theme

--- Lue/layouts/default.vue
+++ Lue/layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-	<div class="w-screen min-h-screen epic xl:overflow-hidden">
+	<div
+		class="w-screen min-h-screen bg-bajaGreen dark:bg-neutral-900 bg-hero-pattern dark:bg-hero-pattern-dark xl:overflow-hidden"
+	>
 		<!-- navbar -->
 		<div
 			class="transition-all ease-in-out duration-150 bg-neutral-50 dark:bg-neutral-900 w-full h-[8vh] border-b-[5px] border-neutral-900 dark:border-neutral-50"
@@ -24,13 +26,7 @@
 <script setup lang="ts"></script>
 
 <style scoped>
-	.epic {
-		background-color: #000000;
-		background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23daff14' fill-opacity='1'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
-	}
-
-	.dark > .epic {
-		background-color: #ffe300;
-		background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23000000' fill-opacity='1'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+	.texture1 {
+		background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23facc15' fill-opacity='0.4'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 	}
 </style>
